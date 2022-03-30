b6eb702c5b792b1d8b51ce959abe0c826a3fd4fd
mfWesterhouse <87536975+mfWesterhouse@users.noreply.github.com>
2022-03-29 21:41:54 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent("car-model", {
+  schema: {
+    modelRef: { type: "string", default: "project151.glb" }
+  },
+  init: function() {
+    this.el.setAttribute("gltf-model", this.data.modelRef);
+    const position = { x: 0, y: 5, z: 80 };
+    const rotation = { x: 0, y: -100, z: 0 };
+    this.el.setAttribute("position", position);
+    this.el.setAttribute("rotation", rotation);
+  }
+});
